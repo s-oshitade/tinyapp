162638a0edc889af4bc1f0fea12c391a6d784702
Seun Oshitade <seun.oshitade@gmail.com>
2022-03-02 17:34:59 +0000

    Add header partial to .ejs files

--- express_server.js
+++ express_server.js
@@ -10,25 +10,13 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
 //route handler for urls
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-//route handler to display single URL and it's shortened form
+//route handler to display single URL and its shortened form
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[this.shortURL] };
   res.render("urls_show", templateVars);
