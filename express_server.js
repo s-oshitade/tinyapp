ce1a5dfd846e08f61f1184e0c5272077d6bae695
Seun Oshitade <seun.oshitade@gmail.com>
2022-03-02 20:58:17 +0000

    Add body parser as dependency and incorporate same into express server

--- express_server.js
+++ express_server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // 
 
+//incorporate body parser to handle inbound data from post route
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 //set ejs as the view engine
 app.set("view engine", "ejs");
 
