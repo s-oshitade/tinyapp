63648e3a2a27929b92188760e593f25391aaeb56
Seun Oshitade <seun.oshitade@gmail.com>
2022-03-02 15:24:18 +0000

    Add route handler for /urls

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // 
 
-//Set ejs as the view engine
+//set ejs as the view engine
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -18,9 +18,16 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+//route handler for urls
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
