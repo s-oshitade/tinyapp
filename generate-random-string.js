4ef25cb672a13a4013ffaa1400d10c7659c3d1c3
Seun Oshitade <seun.oshitade@gmail.com>
2022-03-02 22:42:28 +0000

    Fix bug in function generateRandomString

--- generate-random-string.js
+++ generate-random-string.js
@@ -1,5 +1,6 @@
 /**
  * function returns a string of 6 random alphanumeric characters
+ * @param {number} 
  * @returns {string}
  */
 
@@ -8,7 +9,7 @@ const generateRandomString = (URL_LENGTH) => {
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let result = "";
   const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < URL_LENGTH; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
 
